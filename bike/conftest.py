90b6b7e292ee6f2dbc7d3276a8ccac7b8adf95fa
Kornel <kornel.ro@gmail.com>
2020-06-01 13:41:06 +0200

    snap test

--- bike/conftest.py
+++ bike/conftest.py
@@ -3,10 +3,18 @@ from bike.model.graph import Graph
 from pathlib import Path
 
 
-VERTICES_PATH = Path('bike/test/vertices.csv')
-EDGES_PATH = Path('bike/test/edges.csv')
+VERTICES_BIKES_PATH = Path('bike/test/vertices_test_bikes.csv')
+EDGES_BIKES_PATH = Path('bike/test/edges_test_bikes.csv')
+
+VERTICES_ROADS_PATH = Path('bike/test/vertices_test_roads.csv')
+EDGES_ROADS_PATH = Path('bike/test/edges_test_roads.csv')
 
 
 @fixture(scope='session')
-def graph() -> Graph:
-    return Graph(VERTICES_PATH, EDGES_PATH)
+def graph_bikes() -> Graph:
+    return Graph(VERTICES_BIKES_PATH, EDGES_BIKES_PATH)
+
+
+@fixture(scope='session')
+def graph_roads() -> Graph:
+    return Graph(VERTICES_ROADS_PATH, EDGES_ROADS_PATH)
--- bike/util/test/test_snap.py
+++ bike/util/test/test_snap.py
@@ -1,2 +1,8 @@
-# def test_snap_vertices(roads: Graph):
-#     pass
+from bike.model.graph import Graph
+from bike.util.snap import snap_vertices
+
+
+def test_snap_vertices(graph_roads: Graph, graph_bikes: Graph):
+    graph_bikes = snap_vertices(graph_roads, graph_bikes)
+
+    assert isinstance(graph_bikes, Graph)
